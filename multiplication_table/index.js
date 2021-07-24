45190e06c3c8471fe49110808fa5dd2ac194e0ff
kudoyuto <yuto.kudo@oocl.com>
2021-07-24 16:33:30 +0800

    JS Loop Answers

---
+++ multiplication_table/index.js
@@ -0,0 +1,10 @@
+var str ='';
+for (let i = 1; i<=9; i++){ 
+		for(var j =1; j<=i;j++){
+    	i
+    	str +=  i+'*'+j+'='+ i*j + ' ';
+    }    
+    console.log(str);
+    str='';
+    j =0;    
+}
